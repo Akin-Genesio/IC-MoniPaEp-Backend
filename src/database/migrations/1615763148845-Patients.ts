e71b31892fc95dcf139592c2e93195edef714b56
Vinicius-klaus <vinicius.genesio@usp.br>
2021-03-14 20:28:59 -0300

    feat(patients): create a migration for the table patients

---
+++ src/database/migrations/1615763148845-Patients.ts
@@ -0,0 +1,56 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class Patients1615763148845 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+           new Table({
+               name: "patients",
+               columns: [
+                   {
+                       name: "id",
+                       type: "uuid",
+                       isPrimary: true,
+                   },
+                   {
+                       name: "name",
+                       type: "varchar",
+                   },
+                   {
+                       name: "lastGPSLocation",
+                       type: "varchar",
+                   },
+                   {
+                       name: "allowSMS",
+                       type: "boolean",
+                   },
+                   {
+                       name: "workAddress",
+                       type: "number"
+                   },
+                   {
+                       name: "homeAddress",
+                       type: "number"
+                   },
+                   {
+                       name: "hasHealthPlan",
+                       type: "boolean"
+                   },
+                   {
+                       name: "age",
+                       type: "number"
+                   },
+                   {
+                       name: "status",
+                       type: "varchar"
+                   }
+               ]
+           }) 
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("patients")
+    }
+
+}
