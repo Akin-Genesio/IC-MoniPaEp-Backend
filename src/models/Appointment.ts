34c152210ad758a851ddce49b54bdc4b39a0f9fa
Vinicius-klaus <vinicius.genesio@usp.br>
2021-03-30 19:19:01 -0300

    feat(appointment entity): create appointment entity

---
+++ src/models/Appointment.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { Patient } from "./Patient"
+
+@Entity("appointments")
+class Appointment{
+    @PrimaryGeneratedColumn("uuid")
+    id:string
+
+    @Column()
+    date: Date
+
+    @Column()
+    whenRemember: Date
+
+    @Column()
+    location:string
+
+    @Column()
+    type:string
+
+    @Column()
+    patient_id: string
+
+    @ManyToOne(() => Patient)
+    @JoinColumn({name: "patient_id"})
+    patient: Patient
+}
+
+export {Appointment}
---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+export * from './Patient'
+export * from './Appointment'
+export * from './FAQ'
