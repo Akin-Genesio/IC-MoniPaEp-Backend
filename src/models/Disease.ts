2025ae330d2b7072d6f76e2dc56349b1b5c25519
Vinicius-klaus <vinicius.genesio@usp.br>
2021-03-31 20:27:38 -0300

    feat(disease entity): create disease entity

---
+++ src/models/Disease.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+ 
+ @Entity("Disease")
+ class Disease {
+    @PrimaryColumn()
+    name: string;
+
+    @Column()
+    infected_Monitoring_Days: number;
+
+    @Column()
+    suspect_Monitoring_Days: number;
+
+
+ }
+
+ export{Disease}
