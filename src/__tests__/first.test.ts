3f1122207e95a4af1f8ebc3ede33d965afc22bf5
Vinicius-klaus <vinicius.genesio@usp.br>
2021-04-02 22:14:09 -0300

    feat(test case example): create a test case example

--- src/__tests__/first.test.ts
+++ src/__tests__/first.test.ts
@@ -1,6 +1,28 @@
-describe("First", () => {
-    it("teste", () =>{
+import request from "supertest"
+import {app} from "../app"
 
-        expect((2+2)).toBe(4)
+import createConnection from "../database"
+
+describe("Patient", () => {
+    beforeAll(async () => {
+        const connection = await createConnection()
+        await connection.runMigrations()
+    })
+
+    it("Should create a new patient", async() =>{
+
+        const response = await request(app).post("/patients").send({
+            name: "Akin",
+            email: "akin2@email.com",
+            lastGPSLocation: "123",
+            allowSMS: true,
+            workAddress: "321",
+            homeAddress: "213",
+            hasHealthPlan: false ,
+            age: 23,
+            status: "saudavel"
+        })
+
+        expect(response.status).toBe(201)
     })
 })
