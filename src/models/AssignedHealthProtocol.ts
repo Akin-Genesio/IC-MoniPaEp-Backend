b56eab20c5f9c631ee56822a3271363168156a73
Murillo Mesquita <mu.pracucio@gmail.com>
2021-04-06 22:15:47 -0300

    fix(merging branch): fixed another file for merging

--- src/models/AssignedHealthProtocol.ts
+++ src/models/AssignedHealthProtocol.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, PrimaryColumn } from "typeorm";
+import { Entity, JoinColumn, PrimaryColumn } from "typeorm";
 import { HealthProtocol } from "./HealthProtocol";
 import { Patient } from "./Patient";
 
