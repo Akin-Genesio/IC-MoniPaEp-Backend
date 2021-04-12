7209dc767104c5cc428be7c1d4402e745a0baa25
Vinicius-klaus <vinicius.genesio@usp.br>
2021-04-11 21:57:49 -0300

    test(usm): create tests to success cases

--- src/__tests__/USM/USM.test.ts
+++ src/__tests__/USM/USM.test.ts
@@ -26,5 +26,56 @@ describe("USM", () =>{
 
             expect(response.status).toBe(201)
         })
+
+        it("Should return a list of all the USMs", async () =>{
+            const response = await request(app).get("/usm").send()
+
+            expect(response.status).toBe(200)
+        })
+
+        it("Should return a specific USM", async () =>{
+            const usmRepository = getCustomRepository(USMRepository)
+
+            const newUSM = await request(app).post("/usm").send({
+                name: "getTest",
+                address: "13050023"
+            })
+            
+            const usm = await usmRepository.findOne({name: "getTest"})
+            const response = await request(app).get(`/usm/${usm.name}`).send()
+
+            expect(response.status).toBe(302)
+        })
+
+        it("Should be able to alter a specific USM", async () =>{
+            const usmRepository = getCustomRepository(USMRepository)
+
+            const newUSM = await request(app).post("/usm").send({
+                name: "alterTest",
+                address: "13050023"
+            })
+            
+            const usm = await usmRepository.findOne({name: "alterTest"})
+            const response = await request(app).put(`/usm/${usm.name}`).send({
+                name: "alteredTest",
+                address: "123"
+            })
+
+            expect(response.status).toBe(200)
+        })
+
+        it("Should be able to delete a specific USM", async () =>{
+            const usmRepository = getCustomRepository(USMRepository)
+
+            const newUSM = await request(app).post("/usm").send({
+                name: "deleteTest",
+                address: "13050023"
+            })
+            
+            const usm = await usmRepository.findOne({name: "deleteTest"})
+            const response = await request(app).delete(`/usm/${usm.name}`).send()
+
+            expect(response.status).toBe(200)
+        })
     })
 })
