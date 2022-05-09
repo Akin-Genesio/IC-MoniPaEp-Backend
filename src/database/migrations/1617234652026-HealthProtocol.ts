787137dd4d45a0de7db45c649d11c5dfefd09cce
Murillo Mesquita <mu.pracucio@gmail.com>
2022-05-09 16:39:14 -0300

    chore(none): none

--- src/database/migrations/1617234652026-HealthProtocol.ts
+++ src/database/migrations/1617234652026-HealthProtocol.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class HealthProtocol1617234652026 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
